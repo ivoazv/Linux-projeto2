5a7fde2aa12874312d9cacc09d960c4431ac37d4
ivoazv <98463117+ivoazv@users.noreply.github.com>
2022-08-25 17:51:52 -0300

    Update serv_web.sh
    Correção

--- serv_web.sh
+++ serv_web.sh
@@ -6,3 +6,5 @@ apt-get install unzip -y
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip -d /var/www/html
+mv /var/www/html/linux-site-dio-main/* /var/www/html 
+rm -r /var/www/html/linux-site-dio-main
