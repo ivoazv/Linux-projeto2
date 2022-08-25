1d84c8929e09c87f36801f48bd769e68216fbcda
ivoazv <ivobastos82@gmail>
2022-08-25 15:22:23 -0300

    script de provisionamento de servidor web

---
+++ serv_web.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip -d /var/www/html
